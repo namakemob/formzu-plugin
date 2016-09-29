dc6e55228d146d18c24e4160c04099d7f9dd4b83
namakemob <namakemob@gmail.com>
2016-09-29 17:59:40 +0900

    delete function array dereferencing for PHP 5.3

--- includes/formzu-default-widget.php
+++ includes/formzu-default-widget.php
@@ -38,7 +38,6 @@ class FormzuDefaultWidget extends WP_Widget
     {
         if ( FormzuParamHelper::isset_key($par, 'form_widget_data') ) {
             //$form_id = explode(' ', $par['form_widget_data'])[0];
-            //list($form_id, $h, $m) = explode(' ', $par['form_widget_data']);
             $form_id_height = explode(' ', $par['form_widget_data']);
             $form_id = $form_id_height[0];
         }
