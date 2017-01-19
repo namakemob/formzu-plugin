914f2f6ce0b97b4967c14045d0d2445e77fae6b0
namakemob <namakemob@gmail.com>
2017-01-19 15:05:08 +0900

    fix to output_buffering and unloading functions by .

--- includes/client/_includes-files.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ( ! defined('FORMZU_PLUGIN_PATH') ) {
-    die();
-}
-
-require_once FORMZU_PLUGIN_PATH . '/includes/create_formzu_widget.php';
-require_once FORMZU_PLUGIN_PATH . '/includes/formzu-default-widget.php';
-
